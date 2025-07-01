f4e8f9d8872b48b47238379091fb0a8f3764c55c
emilyrhee <emilyrhee99@gmail.com>
2025-06-30 21:47:36 -0400

    added docstring to StyledCode class

--- src/styled_code.py
+++ src/styled_code.py
@@ -1,6 +1,11 @@
 from manim import Code
 
 class StyledCode(Code):
+    """
+    A class for displaying styled code snippets in the Dracula color theme. The
+    only required argument is a path to a file (str). The default language is
+    Python and can be overridden.
+    """
     def __init__(self, *args, language="python", **kwargs):
         super().__init__(
             *args,
