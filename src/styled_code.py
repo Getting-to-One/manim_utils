013d6491cf318110d2726abf66e541ce884329a4
emilyrhee <emilyrhee99@gmail.com>
2025-07-07 20:25:19 -0400

    made background_stroke_width able to be overriden

--- src/styled_code.py
+++ src/styled_code.py
@@ -3,15 +3,23 @@ from manim import Code
 class StyledCode(Code):
     """
     A class for displaying styled code snippets in the Dracula color theme. The
-    only required argument is a path to a file (str). The default language is
-    Python and can be overridden.
+    only required argument is a path to a file (str).
+    
+    ### Default parameters (can be overriden):
+    language="python"\n
+    background_stroke_width=0
     """
-    def __init__(self, *args, language="python", **kwargs):
+    def __init__(
+        self, *args, 
+        language="python",
+        background_stroke_width=0,
+        **kwargs
+    ):
         super().__init__(
             *args,
             language=language,
             insert_line_no=False,
-            background_stroke_width=0,
+            background_stroke_width=background_stroke_width,
             style=Code.styles_list[10],
             **kwargs
         )
