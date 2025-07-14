35765bd41fbe798c47e1fb6f88279ffac2c7575a
emilyrhee <emilyrhee99@gmail.com>
2025-07-14 11:53:21 -0400

    updated StyledCode class to work for manim 0.19.0

--- src/styled_code.py
+++ src/styled_code.py
@@ -2,24 +2,23 @@ from manim import Code
 
 class StyledCode(Code):
     """
-    A class for displaying styled code snippets in the Dracula color theme. The
-    only required argument is a path to a file (str).
-    
-    ### Default parameters (can be overriden):
-    language="python"\n
-    background_stroke_width=0
+    A class for displaying styled code snippets in the Dracula color theme.
+    Inherits from manim's Code class.
+    The only required argument is a path to a file (str).
     """
     def __init__(
         self, *args, 
         language="python",
-        background_stroke_width=0,
+        has_line_numbers=False,
+        background_config={"stroke_width": 0},
         **kwargs
     ):
         super().__init__(
             *args,
             language=language,
-            insert_line_no=False,
-            background_stroke_width=background_stroke_width,
-            style=Code.styles_list[10],
+            add_line_numbers=has_line_numbers,
+            paragraph_config={"font": "Source Code Pro"}, # cannot be a font with ligatures
+            formatter_style="dracula",
+            background_config=background_config,
             **kwargs
         )
