cc001e6db13a64a89947e97b036dc561f4fd47fa
emilyrhee <emilyrhee99@gmail.com>
2025-07-14 21:59:51 -0400

    added all colors in Dracula palette

--- src/custom_colors.py
+++ src/custom_colors.py
@@ -1,3 +1,13 @@
-from manim import ManimColor
+from manim import ManimColor, RED
 
 DRACULA_BG = ManimColor("#282a36")
+DRACULA_CURRENT_LINE = ManimColor("#44475a")
+DRACULA_FOREGROUND = ManimColor("#f8f8f2")
+DRACULA_COMMENT = ManimColor("#6272a4")
+DRACULA_CYAN = ManimColor("#8be9fd")
+DRACULA_GREEN = ManimColor("#50fa7b")
+DRACULA_ORANGE = ManimColor("#ffb86c")
+DRACULA_PINK = ManimColor("#ff79c6")
+DRACULA_PURPLE = ManimColor("#bd93f9")
+DRACULA_RED = ManimColor("#ff5555")
+DRACULA_YELLOW = ManimColor("#f1fa8c")
